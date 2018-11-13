b7d3a79b02b1eb1e28da1cf575b37cece200254f
RobinMarechal <robin.marechal-2@etu.univ-tours.fr>
2018-11-13 15:31:12 +0100

    init kotlin

--- src/main/java/MainFormula.java
+++
@@ -1,6 +0,0 @@
-public class MainFormula {
-    public static void main(String[] args) {
-        System.out.println(MyMaths.facto(5));
-        System.out.println();
-    }
-}
--- src/main/java/MyMaths.java
+++
@@ -1,9 +0,0 @@
-public class MyMaths {
-    public static int facto(int n){
-        int facto = 0;
-        for(int i=1; i<n; i++){
-            facto = facto*i;
-        }
-        return facto;
-    }
-}
